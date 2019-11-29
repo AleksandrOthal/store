f7d73ec63bec67726d4b0fcc48cd803832fef6da
Alex <alex8graph6@gmail.com>
2019-11-29 22:19:36 +0200

    item

---
+++ item.rb
@@ -0,0 +1,19 @@
+class Item
+
+  attr_reader :name, :prise, :weight
+  attr_writer :name, :prise, :weight
+
+  def initialize(name, prise, weight)
+    @name = name
+    @prise = prise
+    @weight = weight
+  end
+end
+
+
+item1 = Item.new("Margo", 100.0, 55.0)
+puts item1.name + " " + item1.prise.to_s + " $" + item1.weight.to_s + " kg"
+
+item2 = Item.new("Mishel", 10.0, 100.0)
+puts item2.name + " " + "#{item2.prise}" + " #{item2.weight}"
+
