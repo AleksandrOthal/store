a983dede271b64d4916b2228b819cf497fbb0311
Alex <alex8graph6@gmail.com>
2019-12-01 16:40:40 +0200

    fix bag

--- item.rb
+++ item.rb
@@ -12,8 +12,8 @@ end
 
 
 item1 = Item.new("Margo", 100.0, 55.0)
-puts item1.name + " " + item1.prise.to_s + " $" + item1.weight.to_s + " kg"
+puts item1.name + " " + item1.prise.to_s + "$ " + item1.weight.to_s + "kg"
 
 item2 = Item.new("Mishel", 10.0, 100.0)
-puts item2.name + " " + "#{item2.prise}" + " #{item2.weight}"
+puts item2.name + " " + "#{item2.prise}$" + " #{item2.weight}kg"
 
