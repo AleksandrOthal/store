c2ffce4ecef9d403bede504099996618adee2221
Alex <alex8graph6@gmail.com>
2019-12-02 22:31:29 +0200

    part 1

--- item.rb
+++ item.rb
@@ -1,19 +1,15 @@
 class Item
 
-  attr_reader :name, :prise, :weight
-  attr_writer :name, :prise, :weight
+  attr_reader :name, :price, :weight
+  attr_writer :name, :price, :weight
 
-  def initialize(name, prise, weight)
+  def initialize(name, price, weight)
     @name = name
-    @prise = prise
+    @price = price
     @weight = weight
   end
 end
 
 
-item1 = Item.new("Margo", 100.0, 55.0)
-puts item1.name + " " + item1.prise.to_s + "$ " + item1.weight.to_s + "kg"
 
-item2 = Item.new("Mishel", 10.0, 100.0)
-puts item2.name + " " + "#{item2.prise}$" + " #{item2.weight}kg"
 
